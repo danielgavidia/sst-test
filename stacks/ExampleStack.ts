80dfe57b4f1782dc1904e6ce3758ded0e25e7da2
Daniel Gavidia <dgavidia1@gmail.com>
2024-07-03 08:41:51 -0400

    initial commit

---
+++ stacks/ExampleStack.ts
@@ -0,0 +1,38 @@
+import { Api, StaticSite, StackContext, Table } from "sst/constructs";
+
+export function ExampleStack({ stack }: StackContext) {
+    // Create the table
+    const table = new Table(stack, "Counter", {
+        fields: {
+            counter: "string",
+        },
+        primaryIndex: { partitionKey: "counter" },
+    });
+
+    const api = new Api(stack, "Api", {
+        defaults: {
+            function: {
+                bind: [table],
+            },
+        },
+        routes: {
+            "POST /": "packages/functions/src/lambda.handler",
+        },
+    });
+
+    // Deploy our React app
+    const site = new StaticSite(stack, "ReactSite", {
+        path: "packages/frontend",
+        buildCommand: "npm run build",
+        buildOutput: "build",
+        environment: {
+            REACT_APP_API_URL: api.url,
+        },
+    });
+
+    // Show the URLs in the output
+    stack.addOutputs({
+        SiteUrl: site.url,
+        ApiEndpoint: api.url,
+    });
+}
