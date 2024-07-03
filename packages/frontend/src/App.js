949e893bbc148c5eabea81d73e8d1a27a7fabed9
Daniel Gavidia <dgavidia1@gmail.com>
2024-07-03 15:51:28 -0400

    added basic functionality for react app, plus styles

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [count, setCount] = useState(null);
+
+    function onClick() {
+        fetch(process.env.REACT_APP_API_URL, {
+            method: "POST",
+        })
+            .then((response) => response.text())
+            .then(setCount);
+    }
+
+    return (
+        <div className="App">
+            {count && <p>You clicked me {count} times.</p>}
+            <button onClick={onClick}>Click Me!</button>
+        </div>
+    );
 }
-
-export default App;
